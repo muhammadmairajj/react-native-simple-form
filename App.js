34a5cdd591d5e324b813b48f1ea5c415f31fdebd
Muhammad Mairaj <muhammadmairajj@gmail.com>
2023-12-13 00:04:36 +0500

    Modal | Dialog Box

--- App.js
+++ App.js
@@ -1,24 +1,60 @@
-import React, { useState } from 'react';
-import {View, Text, ActivityIndicator, Button} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Modal,
+} from 'react-native';
 
 function App() {
-  const [show, setShow] = useState('');
-
-  const display = () => {
-    setShow(!show);
-
-  }
+  const [showModal, setShowModal] = useState(false);
   return (
-    <View>
+    <View style={styles.main}>
       <Text style={{fontSize: 28, color: 'black', textAlign: 'center'}}>
-        Activity Indicator (Loader)
+        Modal | Dialog Box in react-native
       </Text>
-      <View style={{marginTop: 50}}>
-      {show ? <ActivityIndicator size={70} color={'green'} /> : null}
-      <Button title='SHOW/HIDE LOADER' onPress={display} />
+      <Modal transparent={true} visible={showModal}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello Muhammad Mairaj</Text>
+            <Button title="Close Modal" onPress={() => setShowModal(false)} />
+          </View>
+        </View>
+      </Modal>
+      <View style={styles.buttonView}>
+        <Button title="Open Modal" onPress={() => setShowModal(true)} />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+  },
+  buttonView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    padding: 20
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    padding: 30,
+    borderRadius: 20,
+    backgroundColor: '#fff',
+    shadowColor: 'black',
+    elevation: 5,
+  },
+  modalText: {
+    fontSize: 30,
+    marginBottom: 20,
+  },
+});
 
 export default App;
