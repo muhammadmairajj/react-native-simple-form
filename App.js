2e9099106db6e5372b7e1e83f67ff2f3ad7b6b66
Muhammad Mairaj <muhammadmairajj@gmail.com>
2023-12-12 23:01:53 +0500

    Dynamic Radio Btn

--- App.js
+++ App.js
@@ -1,34 +1,91 @@
-import React from 'react';
-import {View, Text, StyleSheet, SectionList, ScrollView} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TouchableHighlight} from 'react-native';
 
 function App() {
-  const users = [
-    {id: 1, name: 'Mairaj', data: ['PHP', 'React', 'NodeJS']},
-    {id: 2, name: 'Aliahmed', data: ['Angular', 'React', 'NodeJS']},
-    {id: 3, name: 'Arsalan', data: ['PHP', 'Vue', 'Python']},
-    {id: 4, name: 'Sameer', data: ['NextJS', 'React', 'Vue']},
-    {id: 5, name: 'Salman', data: ['Java', '.Net', 'C++']},
-    {id: 6, name: 'Owais', data: ['Python', 'Angular', 'NodeJS']},
+  const [selectedRadioBtn, setSelectedRadioBtn] = useState(1);
+
+  const skills = [
+    {id: 1, name: 'JAVA'},
+    {id: 2, name: 'ReactJS'},
+    {id: 3, name: 'NodeJS'},
+    {id: 4, name: 'Python'},
+    {id: 5, name: 'NestJS'},
   ];
 
   return (
-    <View>
-      <Text style={{fontSize: 26, textAlign: 'center', color: 'black'}}>
-        Section List with example
+    <View style={styles.main}>
+      <Text style={{fontSize: 28, textAlign: 'center'}}>
+        Make radio button in react-native
       </Text>
-     <SectionList
-        sections={users}
-        style={{margin:35}}
-        renderItem={({item}) => (
-          <Text style={{fontSize: 20, marginLeft: 20}}>{item}</Text>
-        )}
-        renderSectionHeader={({section: {name}}) => (
-          <Text style={{fontSize: 25, color: 'red'}}>{name}</Text>
-        )}
-      />
+      
+      <TouchableHighlight onPress={() => setSelectedRadioBtn(1)}>
+        <View style={styles.radioWrapper}>
+          <View style={styles.radio}>
+            {selectedRadioBtn === 1 ? (
+              <View style={styles.radioBg}></View>
+            ) : null}
+          </View>
+          <Text style={styles.radioText}>Radio 1</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight onPress={() => setSelectedRadioBtn(2)}>
+        <View style={styles.radioWrapper}>
+          <View style={styles.radio}>
+            {selectedRadioBtn === 2 ? (
+              <View style={styles.radioBg}></View>
+            ) : null}
+          </View>
+          <Text style={styles.radioText}>Radio 2</Text>
+        </View>
+      </TouchableHighlight>
+
+      <Text style={{fontSize: 28, color: 'black'}}>Dynamic radio button</Text>
+      {skills.map(skill => (
+        <TouchableHighlight
+          onPress={() => setSelectedRadioBtn(skill.id)}
+          key={skill.id}>
+          <View style={styles.radioWrapper}>
+            <View style={styles.radio}>
+              {selectedRadioBtn === skill.id ? (
+                <View style={styles.radioBg}></View>
+              ) : null}
+            </View>
+            <Text style={styles.radioText}>{skill.name}</Text>
+          </View>
+        </TouchableHighlight>
+      ))}
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  radio: {
+    height: 40,
+    width: 40,
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 20,
+    margin: 10,
+  },
+  radioWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  radioText: {
+    fontSize: 20,
+  },
+  radioBg: {
+    backgroundColor: 'black',
+    height: 28,
+    width: 28,
+    borderRadius: 20,
+    margin: 4,
+  },
+});
 
 export default App;
